c283337c8dbbbf9980b07e376b6022d318656cf7
IU Security Club <122540093+IUSecHCMIU@users.noreply.github.com>
2023-11-26 16:26:52 +0700

    Add files via upload

---
+++ Flare-On/flareon9/6/Solution/CodeFile1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using FlareOn;
+
+public class Program
+{
+    // Import user32.dll (containing the function we need) and define
+    // the method corresponding to the native function.
+    //[DllImport("FlareOn.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    //private static extern namespace FlareOn;
+    
+    public static void Main(string[] args)
+    {
+        // Invoke the function as a regular managed method.
+        Flag f = new Flag();
+        string s = f.GetFlag("MyV0ic3!");
+        Console.WriteLine(s);
+    }
+}
