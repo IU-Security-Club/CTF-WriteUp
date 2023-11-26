459b3c54ba9d3533be882f59c3a25da748c41a54
IU Security Club <122540093+IUSecHCMIU@users.noreply.github.com>
2023-11-26 16:24:45 +0700

    Add files via upload

---
+++ Flare-On/flareon9/5/solution/server.py
@@ -0,0 +1,66 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+HOST = '127.0.0.1'
+PORT = 80
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.write_response(b'')
+
+    def do_POST(self):
+        content_length = int(self.headers.get('content-length', 0))
+        mess = self.rfile.read(content_length)
+        print(mess,'\n')
+        
+        resp = b''
+        if mess == b'y\x00d\x00N\x008\x00B\x00X\x00q\x001\x006\x00R\x00E\x00=\x00':
+            resp = b"TdQdBRa1nxGU06dbB27E7SQ7TJ2+cd7zstLXRQcLbmh2nTvDm1p5IfT/Cu0JxShk6tHQBRWwPlo9zA1dISfslkLgGDs41WK12ibWIflqLE4Yq3OYIEnLNjwVHrjL2U4Lu3ms+HQc4nfMWXPgcOHb4fhokk93/AJd5GTuC5z+4YsmgRh1Z90yinLBKB+fmGUyagT6gon/KHmJdvAOQ8nAnl8K/0XG+8zYQbZRwgY6tHvvpfyn9OXCyuct5/cOi8KWgALvVHQWafrp8qB/JtT+t5zmnezQlp3zPL4sj2CJfcUTK5copbZCyHexVD4jJN+LezJEtrDXP1DJNg=="
+
+        self.write_response(resp)
+
+    def write_response(self, content):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(content)
+
+        print(self.headers)
+        print(content.decode('utf-8'))
+
+print(f"{HOST} {PORT}")
+httpd = HTTPServer((HOST, PORT), SimpleHTTPRequestHandler)
+httpd.serve_forever()
+
+
+"""
+POST / HTTP/1.1
+Connection: Keep-Alive
+User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; 11950)
+Content-Length: 24
+Host: flare-on.com
+
+ydN8BXq16RE= 
+
+HTTP/1.0 200 OK
+Server: Apache On 9 
+Date: Tue, 14 Jun 2022 16:14:36 GMT
+
+TdQdBRa1nxGU06dbB27E7SQ7TJ2+cd7zstLXRQcLbmh2nTvDm1p5IfT/Cu0JxShk6tHQBRWwPlo9zA1dISfslkLgGDs41WK12ibWIflqLE4Yq3OYIEnLNjwVHrjL2U4Lu3ms+HQc4nfMWXPgcOHb4fhokk93/AJd5GTuC5z+4YsmgRh1Z90yinLBKB+fmGUyagT6gon/KHmJdvAOQ8nAnl8K/0XG+8zYQbZRwgY6tHvvpfyn9OXCyuct5/cOi8KWgALvVHQWafrp8qB/JtT+t5zmnezQlp3zPL4sj2CJfcUTK5copbZCyHexVD4jJN+LezJEtrDXP1DJNg==
+
+
+
+
+
+POST / HTTP/1.1
+Connection: Keep-Alive
+User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; CLR)
+Content-Length: 16
+Host: flare-on.com
+
+VYBUpZdG  
+
+HTTP/1.0 200 OK
+Server: Apache On 9 
+Date: Tue, 14 Jun 2022 16:14:36 GMT
+
+F1KFlZbNGuKQxrTD/ORwudM8S8kKiL5F906YlR8TKd8XrKPeDYZ0HouiBamyQf9/Ns7u3C2UEMLoCA0B8EuZp1FpwnedVjPSdZFjkieYqWzKA7up+LYe9B4dmAUM2lYkmBSqPJYT6nEg27n3X656MMOxNIHt0HsOD0d+
+"""
